ed0f077da86dabe4dfa3179ab069b1713028ceff
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-10-18 11:42:32 +0300

    webpack configure + install mui + chartjs + dayjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import HomePage from "./components/HomePage";
-import React from "react";
+import React, { useState } from "react";
 import { Box, TextField, Grid, Button } from "@mui/material/";
-// import RangeDatePicker from "./components/RangeDatePicker";
+import RangeDatePicker from "./components/RangeDatePicker";
 
 export default function App() {
+  const [fetchData, setFetchData] = useState([]);
+  function handleGenerateClick(fromDate, toDate, word) {
+    setFetchData([fromDate, toDate, word]);
+  }
+
+  console.log(fetchData);
+
   return (
     <>
       <h1>Hello senti!</h1>
-
-      {/* <RangeDatePicker onRangeSelect={handleGenerateClick} /> */}
+      <p>test</p>
+      <RangeDatePicker onRangeSelect={handleGenerateClick} />
     </>
   );
 }
