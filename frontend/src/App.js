ccb27985a87c4cc7cc9ab2afb72dd59a8149ef3e
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-10-18 21:54:45 +0300

    working draft with hardcode fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import HomePage from "./components/HomePage";
 import React, { useState, useEffect } from "react";
-import { Grid, Container, Typography } from "@mui/material/";
+import { Grid, Container, Typography, Button } from "@mui/material/";
 import RangeDatePicker from "./components/RangeDatePicker";
 import LineChart from "./components/LineChart";
 import { TestData } from "./components/TestData";
@@ -9,23 +9,28 @@ import "../src/App.css";
 export default function App() {
   const [fetchData, setFetchData] = useState([]);
   const [testData, setTestData] = useState({});
+  const [sentiData, setSentiData] = useState([]);
 
   function handleGenerateClick(fromDate, toDate, word) {
     setFetchData([fromDate, toDate, word]);
   }
 
+  //date sentiment
+
+  //вариант решения: две кнопки - choose date & show sentiment
+  //while date is not picked yet show sentiment is disabled
+
   useEffect(() => {
     if (typeof fetchData[0] == "string") {
-      fetch("https://jsonplaceholder.typicode.com/todos/1")
+      fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
         .then((response) => response.json())
-        .then((json) => console.log(json))
-        .then(() => {
+        .then((json) => {
           setTestData({
-            labels: TestData.map((data) => data.year),
+            labels: json.map((uniqYear) => uniqYear.date),
             datasets: [
               {
-                label: "Users Gained",
-                data: TestData.map((data) => data.userGain),
+                label: "sentiment to the word according to New York Times",
+                data: json.map((uniqYear) => uniqYear.sentiment),
                 backgroundColor: [
                   "rgba(75,192,192,1)",
                   "#ecf0f1",
@@ -42,7 +47,27 @@ export default function App() {
     }
   }, [fetchData]);
 
+  // useEffect(() => {
+  //   if (typeof fetchData[0] == "string") {
+  //     console.log(typeof fetchData[0]);
+  //     fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
+  //       .then((response) => response.json())
+  //       .then((json) => console.log(json));
+  //   }
+  // }, [fetchData]);
+
+  // function handleTestFetch() {
+  //   console.log("handleTestFetch is working");
+  //   fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
+  // }
+
   console.log(fetchData);
+
+  // fetch(
+  //   "/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01"
+  // )
 
   return (
     <Container maxWidth="md" sx={{ bgcolor: "white", minHeight: "100vh" }}>
