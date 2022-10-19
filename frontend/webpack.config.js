6b4340b5a8b4a2bbec9797cad8606a85f7ac3ae4
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-10-19 18:51:01 +0300

    version 1.0.0 (must fix fetch bug)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    filename: "sentiapp.js",
+    filename: "bundle.js",
   },
   module: {
     rules: [
