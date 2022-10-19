46b6d3fcd15348f4c90cdf777c2ec780cce5a182
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-10-19 18:51:01 +0300

    version 1.0.0 (must fix fetch bug)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,25 @@
-import HomePage from "./components/HomePage";
 import React, { useState, useEffect } from "react";
 import { Grid, Container, Typography, Button } from "@mui/material/";
 import RangeDatePicker from "./components/RangeDatePicker";
 import LineChart from "./components/LineChart";
-import { TestData } from "./components/TestData";
 import "../src/App.css";
 
 export default function App() {
   const [fetchData, setFetchData] = useState([]);
-  const [testData, setTestData] = useState({});
-  const [sentiData, setSentiData] = useState([]);
+  const [sentiData, setSentiData] = useState({});
 
   function handleGenerateClick(fromDate, toDate, word) {
     setFetchData([fromDate, toDate, word]);
   }
 
-  //date sentiment
-
-  //вариант решения: две кнопки - choose date & show sentiment
-  //while date is not picked yet show sentiment is disabled
-
   useEffect(() => {
+    const url = `/api?keyword=${fetchData[2]}&start-date=${fetchData[0]}&end-date=${fetchData[1]}`;
     if (typeof fetchData[0] == "string") {
-      fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
+      fetch(url)
         .then((response) => response.json())
         .then((json) => {
-          setTestData({
+          console.log(json);
+          setSentiData({
             labels: json.map((uniqYear) => uniqYear.date),
             datasets: [
               {
@@ -47,28 +41,6 @@ export default function App() {
     }
   }, [fetchData]);
 
-  // useEffect(() => {
-  //   if (typeof fetchData[0] == "string") {
-  //     console.log(typeof fetchData[0]);
-  //     fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
-  //       .then((response) => response.json())
-  //       .then((json) => console.log(json));
-  //   }
-  // }, [fetchData]);
-
-  // function handleTestFetch() {
-  //   console.log("handleTestFetch is working");
-  //   fetch("/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01")
-  //     .then((response) => response.json())
-  //     .then((json) => console.log(json));
-  // }
-
-  console.log(fetchData);
-
-  // fetch(
-  //   "/api?keyword=ukraine&start-date=2020-01-01&end-date=2020-09-01"
-  // )
-
   return (
     <Container maxWidth="md" sx={{ bgcolor: "white", minHeight: "100vh" }}>
       <Grid container>
@@ -76,13 +48,13 @@ export default function App() {
           <RangeDatePicker onRangeSelect={handleGenerateClick} />
         </Grid>
         <Grid item xs={12}>
-          {Object.keys(testData).length === 0 &&
-          testData.constructor === Object ? (
+          {Object.keys(sentiData).length === 0 &&
+          sentiData.constructor === Object ? (
             <Typography variant="h4" align="center">
-              Choose dates and word you are looking for{" "}
+              Choose dates and word you are looking for
             </Typography>
           ) : (
-            <LineChart chartData={testData} />
+            <LineChart chartData={sentiData} />
           )}
         </Grid>
       </Grid>
