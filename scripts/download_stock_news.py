5e3646a9bbf77057247a3ba2d843e6303c50bd8c
Bohdan Garchu <bohdan.garchu@gmail.com>
2022-11-29 18:51:59 +0100

    add sources support

--- scripts/download_stock_news.py
+++ scripts/download_stock_news.py
@@ -1,6 +1,8 @@
 import requests
 from datetime import datetime
 import time
+import json
+import os
 from typing import Optional
 
 from django.db import transaction
@@ -12,13 +14,60 @@ URL_ALL = 'https://api.tickertick.com/feed?n=1000'
 
 def run():
     start = time.time()
-    # tickers = ['jnj', 'goog', 'msft', 'fb', 'mrk', 'aapl', 'twtr', 'tsm', 'nflx', 'tsla', 'amzn', 'pfe']
-    tickers = ['nvda']
-    download_articles_for_tickers(tickers)
+    # tickers = load_tickers()
+    tickers = ['nio']
+    download_articles(tickers)
     end = time.time()
     print(f'startup script finished, total instances in the db: {len(FinancialArticle.objects.all())}')
     print('execution time: ' + str(round((end - start) / 60)) + ' minutes')
     exit()
+
+
+def download_all(base_url=URL_ALL):
+    print(f'downloading stock news from {base_url}...')
+    last_id = None
+    while True:
+        if last_id:
+            articles = fetch_older_articles(last_id, base_url)
+        else:
+            articles = fetch_articles(base_url)
+        if articles is None:
+            time.sleep(2)
+            continue
+        elif len(articles) == 0:
+            print(f'all articles downloaded. lastId={last_id}')
+            time.sleep(2)
+            break
+        save_articles(articles)
+        last_id = articles[-1]['id']
+        time.sleep(2)
+
+
+def download_articles(tickers: list[str]):
+    for ticker in tickers:
+        for source in load_sources():
+            url = f'{URL_ALL}&q=(and tt:{ticker} s:{source})'
+            download_all(base_url=url)
+
+
+def fetch_articles(url: str) -> Optional[list[dict]]:
+    response = None
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return list(data['stories'])
+    except Exception as e:
+        if response:
+            print(f'exception for response {response.text}: {e}')
+        else:
+            print(e)
+        return None
+
+
+def fetch_older_articles(last_id: str, base_url: str) -> list[dict]:
+    """fetch articles older than last_id"""
+    url = f'{base_url}&last={last_id}'
+    return fetch_articles(url)
 
 
 def save_articles(articles: list[dict]):
@@ -27,56 +76,13 @@ def save_articles(articles: list[dict]):
         entry.save()
 
 
-def download_all(base_url=URL_ALL):
-    print('downloading stock news...')
-    last_id = None
-    while True:
-        if last_id:
-            articles = fetch_older_articles(last_id, base_url)
-        else:
-            articles = fetch_articles(base_url)
-        if articles is None:
-            continue
-        elif len(articles) == 0:
-            print(f'all articles downloaded. lastId={last_id}')
-            break
-        save_articles(articles)
-        last_id = articles[-1]['id']
-        time.sleep(2)
+def load_sources() -> list[str]:
+    file = open(os.path.join(os.path.dirname(__file__), 'top_news_sources.json'))
+    return json.load(file)['sources']
 
 
-def download_articles_for_tickers(tickers: list[str]):
-    for ticker in tickers:
-        url = f'{URL_ALL}&q=tt:{ticker}'
-        print(f'downloading articles for ticker {ticker}...')
-        download_all(base_url=url)
+def load_tickers() -> list[str]:
+    file = open(os.path.join(os.path.dirname(__file__), 'tickers.json'))
+    return json.load(file)['tickers']
 
 
-def fetch_articles(url: str) -> Optional[list[dict]]:
-    try:
-        response = requests.get(url)
-        data = response.json()
-        return list(data['stories'])
-    except Exception as e:
-        print(f'exception for response {response.text}: {e}')
-        return None
-
-
-def fetch_older_articles(article_id: str, base_url: str) -> list[dict]:
-    """fetch articles older than article_id"""
-    url = f'{base_url}&last={article_id}'
-    return fetch_articles(url)
-
-
-def fetch_last_x_articles(x: int, last_id=None) -> list[dict]:
-    if x < 1000:
-        return fetch_articles(URL_ALL)
-    articles = []
-    for i in range(int(x / 1000)):
-        if last_id:
-            articles.extend(fetch_older_articles(last_id))
-        else:
-            articles.extend(fetch_articles(URL_ALL))
-        last_id = articles[-1]['id']
-        time.sleep(2)
-    return articles
