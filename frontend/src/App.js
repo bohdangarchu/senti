1cde2e900fb3e836b12568a57c6d0855e3af3862
mishaaaaaaa <misharudenkomisha@gmail.com>
2022-11-30 18:07:59 +0200

    added header; added basic routing; refactored app for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,118 +1,20 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Grid, Container, Typography, Box } from "@mui/material/";
-import Article from "./components/Article";
-import CircularProgress from "@mui/material/CircularProgress";
-import RangeDatePicker from "./components/RangeDatePicker";
-import "../src/App.css";
-import useFetch from "./hooks/useFetch";
-import { Line, getElementAtEvent } from "react-chartjs-2";
-import Chart from "chart.js/auto";
-import { options } from "./components/options";
+import React from "react";
+import General from "./pages/Generals";
+import Financials from "./pages/Financials";
+import Header from "./components/Header";
+import { Container } from "@mui/material/";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
-  const [fetchData, setFetchData] = useState([]);
-  const [sentiData, setSentiData] = useState({});
-  const [detailsOnDate, setDetailsOnDate] = useState([]);
-  const [sentiDataDetails, setSentiDataDetails] = useState([]);
-  const { get, loading } = useFetch(`/api`);
-  const chartRef = useRef();
-
-  function handleGenerateClick(fromDate, toDate, word) {
-    setFetchData([fromDate, toDate, word]);
-  }
-
-  const handlePointDate = (event) => {
-    const point = getElementAtEvent(chartRef.current, event);
-    const pointIndex = point[0].index;
-    const pointDate = sentiData.labels[pointIndex];
-    const yearAndMonth = pointDate.split("-");
-    const sentiWord = fetchData[2];
-    setDetailsOnDate([yearAndMonth[0], yearAndMonth[1], sentiWord]);
-  };
-
-  useEffect(() => {
-    setSentiDataDetails({});
-    setSentiData({});
-    if (typeof fetchData[0] == "string") {
-      get(
-        `?keyword=${fetchData[2]}&start-date=${fetchData[0]}&end-date=${fetchData[1]}`
-      ).then((json) => {
-        setSentiData({
-          labels: json.map((uniqYear) => uniqYear.date),
-          datasets: [
-            {
-              label: "click on the point to see details",
-              data: json.map((uniqYear) => uniqYear.sentiment),
-            },
-          ],
-        });
-      });
-    }
-  }, [fetchData]);
-
-  useEffect(() => {
-    if (typeof detailsOnDate[0] == "string") {
-      get(
-        `/most-negative-articles/${detailsOnDate[0]}/${detailsOnDate[1]}?keyword=${detailsOnDate[2]}`
-      ).then((json) => {
-        setSentiDataDetails(json);
-      });
-    }
-  }, [detailsOnDate]);
-
-  console.log(sentiDataDetails, sentiDataDetails.length);
-
   return (
-    <Container maxWidth="md" sx={{ bgcolor: "white", minHeight: "100vh" }}>
-      <Grid container>
-        <Grid item xs={12}>
-          <RangeDatePicker onRangeSelect={handleGenerateClick} />
-        </Grid>
-        <Grid item xs={12}>
-          {Object.keys(sentiData).length === 0 &&
-          sentiData.constructor === Object ? (
-            <Typography variant="h4" align="center" gutterBottom>
-              Choose dates and word you are looking for!
-            </Typography>
-          ) : (
-            <Line
-              ref={chartRef}
-              data={sentiData}
-              onClick={handlePointDate}
-              options={options}
-            />
-          )}
-          <Box
-            sx={{ display: "flex" }}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            mt={5}
-          >
-            {loading && <CircularProgress size={100} />}
-          </Box>
-        </Grid>
-        <Grid item xs={12}>
-          <Box sx={{ display: "flex", flexDirection: "column" }} mt={2}>
-            {sentiDataDetails.length > 0 && loading == false ? (
-              <Box>
-                <Typography variant="h5" align="left" gutterBottom>
-                  These are the most negative articles affected the sentiment
-                </Typography>
-                {sentiDataDetails.map((article) => (
-                  <Article
-                    key={sentiDataDetails.indexOf(article)}
-                    date={article.date}
-                    sentiment={article.sentiment}
-                    text={article.text}
-                    url={article.url}
-                  />
-                ))}
-              </Box>
-            ) : null}
-          </Box>
-        </Grid>
-      </Grid>
-    </Container>
+    <>
+      <Header />
+      <Container maxWidth="md">
+        <Routes>
+          <Route path="/" element={<General />} />
+          <Route path="/financial" element={<Financials />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
-createRoot(document.querySelector("#app")).render(<App />);
+import { BrowserRouter } from "react-router-dom";
+createRoot(document.querySelector("#app")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
